b79e341d63fb373c3b49ddeefdbfb3072bcd9076
kamalesh <kamaleshkumark25@gmail.com>
2022-12-16 11:27:15 +0530

    added reverepair count program

--- ReversePath.java
+++ ReversePath.java
@@ -6,6 +6,7 @@ class Reverse{
         int arr[] = new int[n];
         for(int i=0;i<n;i++)
             arr[i]=sc.nextInt();
+            
         int a=0;
         for(int i=0;i<n;i++)
             for(int j=i+1;j<n;j++)
